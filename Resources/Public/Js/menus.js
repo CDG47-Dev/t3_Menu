f5abd17cff10c91feb384f5c913d33a33bc84b34
CDG47 <n8F6NHoW>
2016-11-24 14:18:05 +0100

    Update  Je suis

--- Resources/Public/Js/menus.js
+++ Resources/Public/Js/menus.js
@@ -2,7 +2,8 @@ $(document).ready(function()
 {
         $('.jesuis #select li').click(function()
     {
-                var selection = $('.jesuis #select li').attr('id');
+                
+				var selection = $(this).attr('id');
                 var liste = '.jesuis div[id="liste'+selection+'"]';
                 $('.jesuis div[id^="liste"] ').attr("style","display:none;");
                 $( liste ).attr("style","display:inline-block;");
@@ -12,6 +13,7 @@ $(document).ready(function()
 		$('.jesuis .dropdown-menu li').click(function(){
 			var selText = $(this).text();
 			$(this).parents('#select').find('.dropdown-toggle').html('<span class="selected entete">'+selText+'</span> <span class="caret"></span>');
+			$(this).parents('#select').find('.dropdown-toggle').addClass('selected');
 		});
 
 }
